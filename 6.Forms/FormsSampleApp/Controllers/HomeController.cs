e7d9f19cbb06410cec10133c196e624e2fd9e33c
Christofer Castañeda <christocastanedac@gmail.com>
2025-09-28 19:31:50 -0700

    7 Persistence

---
+++ 6.Forms/FormsSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using FormsSampleApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormsSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(GreetingViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return BadRequest();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
